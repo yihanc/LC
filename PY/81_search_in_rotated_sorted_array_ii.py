e57dfece164bc3e0180355befadc9cf0184d5bf0
Shiyan Cao <shawncaojob@gmail.com>
2018-10-27 16:04:24 -0700

    Update 81_search_in_rotated_sorted_array_ii.py

--- PY/81_search_in_rotated_sorted_array_ii.py
+++ PY/81_search_in_rotated_sorted_array_ii.py
@@ -9,31 +9,7 @@
 # 
 # Subscribe to see which companies asked this question
 
-# 2017.03.04 Rewrite in my style
-class Solution(object):
-    def search(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        if not nums: return -1
-        n = len(nums)
-        l, r = 0, n - 1
-        while l < r:
-            mid = (l + r) //2
-            if nums[mid] == target:
-                return mid
-            elif ((nums[mid] > nums[r] and (target > nums[mid] or target <= nums[r]))
-                or (nums[mid] < nums[r] and target > nums[mid] and target <= nums[r])):
-                l = mid + 1
-            else:
-                r = mid - 1
-            
-        return l if nums[l] == target else -1
-        
-
-# 2017.02.25 One template for all.
+# 2018, Same
 class Solution(object):
     def search(self, nums, target):
         """
@@ -41,24 +17,25 @@ class Solution(object):
         :type target: int
         :rtype: bool
         """
-        if not nums: return False
-        n = len(nums)
-        l, r = 0, n - 1
+        l = 0
+        r = len(nums) - 1
+        
         while l <= r:
-            if l == r and nums[l] != target:
-                return False
+            m = (l + r) // 2
             
-            mid = (l + r) // 2
-            
-            if nums[mid] == target:
+            if nums[m] == target:
                 return True
-            elif nums[mid] == nums[r]:
-                r -= 1
-            elif (( nums[mid] < nums[r] and target > nums[mid] and target <= nums[r] )
-                or ( nums[mid] > nums[r] and (target > nums[mid] or target <= nums[r]))):
-                l = mid + 1
+            
+            while m < r and nums[m] == nums[r]:     # Only differences compare to rotated I
+                r -= 1            
+            
+            if (nums[m] < target <= nums[r] or 
+                ( nums[m] > nums[r] and (target > nums[m] or target <= nums[r]) )):
+                l = m + 1
             else:
-                r = mid
+                r = m - 1
+        return False    
+            
 
 # Similar to Question 33
 # Only check nums[l] and nums[r]
