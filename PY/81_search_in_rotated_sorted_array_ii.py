4521d27bd4d7960e714e3922ea373d3cf1cdbfcf
Shiyan Cao <shawncaojob@gmail.com>
2018-10-27 17:33:00 -0700

    Update 81_search_in_rotated_sorted_array_ii.py

--- PY/81_search_in_rotated_sorted_array_ii.py
+++ PY/81_search_in_rotated_sorted_array_ii.py
@@ -9,6 +9,25 @@
 # 
 # Subscribe to see which companies asked this question
 
+# 2018.10.27 Di
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        l, r = 0, len(nums) - 1
+        
+        while l < r:
+            m = (l + r) // 2
+     
+            if nums[m] > nums[r]:
+                l = m + 1
+            else:
+                r = m
+        return nums[l]
+            
+
 # 2018, Same
 class Solution(object):
     def search(self, nums, target):
