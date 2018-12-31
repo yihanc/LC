d1fa22babc815ca28ff286f1e5cdc839fc162295
yihanc <sherrydishao@gmail.com>
2018-12-31 10:54:01 -0800

    Update 301_remove_invalid_parentheses.py

--- PY/301_remove_invalid_parentheses.py
+++ PY/301_remove_invalid_parentheses.py
@@ -17,8 +17,35 @@
 # MLE Case: ")k)))())()())))())"
 # Wrong case: "())(((()m)("
 
-# 2017.03.25 BFS
+# 2018.12.31
+class Solution(object):
+    def removeInvalidParentheses(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        res = []
+        self.reverse(res, s, 0, 0, ["(", ")"])
+        return res
 
+    def reverse(self, res, s, last_i, last_j, par):
+        count = 0
+        for i in xrange(last_i, len(s)):
+            if s[i] == par[0]: count += 1
+            if s[i] == par[1]: count -= 1
+            if count >= 0: continue
+            
+            for j in xrange(last_j, i + 1):
+                if s[j] == par[1] and (j == last_j or s[j-1] != par[1]):
+                    self.reverse(res, s[:j] + s[j+1:], i, j, par)
+            return
+    
+        s = s[::-1]
+        if par[0] == "(":
+            self.reverse(res, s, 0, 0, [par[1], par[0]])
+        else:
+            res.append(s)
+    
 
 # 2017.0318 DFS + Reverse, Fast but tricky algorithm
 class Solution(object):
